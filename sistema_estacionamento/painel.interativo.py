dc07c6bd64ecc77095f1f6520475e3d321447b47
daks-2k25 <thiagofreitasdelima9@gmail.com>
2026-06-09 23:13:40 -0300

    Atualiza sistema de estacionamento

---
+++ sistema_estacionamento/painel.interativo.py
@@ -0,0 +1,14 @@
+while True:
+    print("-"*30)
+    print("Escolha uma das opçoes abaixo")
+    print()
+    print("1 - Entrada de veiculos")
+    print("2 - Saida de veiculos")
+    print("3 - Listar veiculos selecionados")
+    print("4 - Consultar vagas disponiveis")
+    print("5 - Sair")
+    print()
+    print("-"*30)
+    escolha = input("Opçao deseja: ")
+    break
+
